40c6f63308125981e137592d016d99a914c04790
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-04-11 19:02:47 -0300

    preparing project setup

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      backgroud: '#FAFAFA',
+      card: '#F3F2F2',
+      button: '#E2E8F0',
+      label: '#8D8686',
+      text: '#574F4D',
+      subtitle: '#403937',
+      title: '#272221',
+
+      red: '#EF4444',
+      orange: '#F97316',
+      green: '#4ADE80',
+
+      'button-dark': '#E4E4E7',
+      'red-dark': '#DC2626',
+      'red-light': '#FEE2E2',
+      'red-border': '#FECACA',
+      'orange-light': '#FFEDD5'
+    },
+    extend: {
+      fontFamily: {
+        heading: "'Poppins', sans-serif;",
+        body: "'Roboto', sans-serif"
+      }
+    }
+  },
+  plugins: []
+};
