0ab6de56626ec0d17daae6ad09e59bf6585263a1
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2023-04-19 19:10:46 -0300

    refactor: change logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import PizzaIcon from '../../public/icons/pizza.svg';
+import LogoImg from '../../public/logo.svg';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex gap-2">
-      <Image src={PizzaIcon} alt="Ícone de pizza" />
-
-      <h1 className="text-red text-3xl font-extrabold">
-        Hot<span className="text-subtitle">Pizza</span>
-      </h1>
+    <Link href="/">
+      <Image src={LogoImg} alt="Logo do HotPizza" />
     </Link>
   );
 }
