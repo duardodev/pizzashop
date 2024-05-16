a8219ba052b365dfd659f45d43688ab53a1297e4
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-05-16 20:32:29 -0300

    feat: add verification and change the style

--- src/components/SelectedPizzas.tsx
+++ src/components/SelectedPizzas.tsx
@@ -2,79 +2,68 @@
 
 import { useCart } from '@/hooks/useCart';
 import { SelectedPizza } from './SelectedPizza';
-import Modal from './Modal';
-
-import { ImSpinner8 } from 'react-icons/im';
+import Link from 'next/link';
 
 const deliveryPrice = 8;
 
-interface SelectedPizzasProps {
-  modalIsVisible: boolean;
-  isLoading: boolean;
-}
-
-export function SelectedPizzas({ modalIsVisible, isLoading }: SelectedPizzasProps) {
+export function SelectedPizzas() {
   const { cartItems, cartItemsTotal, cartQuantity } = useCart();
   const cartTotal = deliveryPrice + cartItemsTotal;
 
   const formattedDeliveryPrice = deliveryPrice.toLocaleString('pt-BR', {
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
   });
 
   const formattedItemsTotal = cartItemsTotal.toLocaleString('pt-BR', {
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
   });
 
   const formattedCartTotal = cartTotal.toLocaleString('pt-BR', {
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
   });
 
   return (
-    <div className="w-[620px] lg:w-[420px] flex flex-col items-start gap-3">
-      <h1 className="text-title text-xl font-extrabold">Resumo do pedido</h1>
+    <div className="w-full h-full flex flex-col justify-between gap-10">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-title text-xl font-extrabold">Pizzas selecionadas</h1>
 
-      <div className="w-full bg-card rounded-lg mx-auto p-7 flex flex-col gap-6">
-        {cartItems.map(item => {
-          return <SelectedPizza key={item.slug} pizza={item} />;
-        })}
+        {cartQuantity >= 1 ? (
+          <>
+            <div className="max-h-[380px] overflow-y-auto space-y-8">
+              {cartItems.map(item => {
+                return <SelectedPizza key={item.slug} pizza={item} />;
+              })}
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center justify-between">
+                <p>Entrega</p>
+                <p className="text-sm">R$ {formattedDeliveryPrice}</p>
+              </div>
 
-        <div className="flex flex-col gap-3">
-          <div className="flex items-center justify-between">
-            <p>Entrega</p>
-            <p className="text-sm">R$ {formattedDeliveryPrice}</p>
-          </div>
+              <div className="flex items-center justify-between">
+                <p>Subtotal</p>
+                <p className="text-sm">R$ {formattedItemsTotal}</p>
+              </div>
 
-          <div className="flex items-center justify-between">
-            <p>Subtotal</p>
-            <p className="text-sm">R$ {formattedItemsTotal}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="text-lg font-bold">Total</p>
-            <p className="font-bold">R$ {formattedCartTotal}</p>
-          </div>
-        </div>
-
-        {isLoading ? (
-          <button
-            type="submit"
-            disabled={cartQuantity <= 0}
-            className="h-11 bg-red text-white text-sm font-bold uppercase rounded-md py-3 px-2 flex items-center justify-center enabled:hover:bg-red-dark transition-colors duration-300 disabled:opacity-70 disabled:cursor-not-allowed"
-          >
-            <ImSpinner8 size={18} className="animate-spin" />
-          </button>
+              <div className="flex items-center justify-between">
+                <p className="text-lg font-bold">Total</p>
+                <p className="font-bold">R$ {formattedCartTotal}</p>
+              </div>
+            </div>
+          </>
         ) : (
-          <button
-            type="submit"
-            disabled={cartQuantity <= 0}
-            className="h-11 bg-red text-white text-sm font-bold uppercase rounded-md py-3 px-2 enabled:hover:bg-red-dark transition-colors duration-300 disabled:opacity-70 disabled:cursor-not-allowed"
-          >
-            Finalizar pedido
-          </button>
+          <p className="text-center">Nenhuma pizza selecionada.</p>
         )}
-
-        {modalIsVisible ? <Modal /> : ''}
       </div>
+
+      {cartQuantity >= 1 && (
+        <Link
+          href="/checkout"
+          className="h-11 bg-red text-white text-sm font-bold uppercase rounded-md py-3 px-2 flex items-center justify-center hover:bg-red-dark transition-colors duration-300 disabled:opacity-70 disabled:cursor-not-allowed"
+        >
+          Confirmar perdido
+        </Link>
+      )}
     </div>
   );
 }
