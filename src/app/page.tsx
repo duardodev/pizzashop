c341edb47b6b2fa6dd2e063da6717f90fac62a82
Deivit Eduardo <deiviteduardo87@gmail.com>
2024-08-20 19:55:40 -0300

    refactor: move getPizzasData function to Menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,22 @@
 import { Metadata } from 'next';
-
 import { Intro } from '@/components/Intro';
 import { Divider } from '@/components/Divider';
 import { AboutUs } from '@/components/AboutUs';
 import { Menu } from '@/components/Menu';
 import { Footer } from '@/components/Footer';
 
-import { fetchHygraphQuery } from '@/utils/fetch-hygraph-query';
-import { Pizza } from '@/types/pizza';
-
-interface PizzasData {
-  pizzas: Pizza[];
-}
-
 export const metadata: Metadata = {
-  title: 'Home | PizzaShop'
+  title: 'Home | PizzaShop',
 };
 
 export default async function Home() {
-  const getPizzasData = async (): Promise<PizzasData> => {
-    const query = `
-      query PizzasQuery {
-        pizzas {
-          title
-          description
-          slug
-          minimumPrice
-          mediumPrice
-          maximumPrice
-          image {
-            url
-          }
-        }
-      } 
-    `;
-
-    return fetchHygraphQuery(query);
-  };
-
-  const { pizzas } = await getPizzasData();
-
   return (
     <>
       <Intro />
-
       <Divider gradientTo="left" />
-
       <AboutUs />
-
       <Divider gradientTo="right" />
-
-      <Menu pizzas={pizzas} />
-
+      <Menu />
       <Footer />
     </>
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,44 +1,41 @@
-'use client';
-
 import { Pizza } from '@/types/pizza';
 import { PizzaCard } from './PizzaCard';
-import { motion } from 'framer-motion';
+import { fetchHygraphQuery } from '@/utils/fetch-hygraph-query';
 
-interface MenuProps {
+interface PizzasData {
   pizzas: Pizza[];
 }
 
-export function Menu({ pizzas }: MenuProps) {
+export async function Menu() {
+  const getPizzasData = async (): Promise<PizzasData> => {
+    const query = `
+      query PizzasQuery {
+        pizzas {
+          title
+          description
+          slug
+          minimumPrice
+          mediumPrice
+          maximumPrice
+          image {
+            url
+          }
+        }
+      }
+    `;
+
+    return fetchHygraphQuery(query);
+  };
+
+  const { pizzas } = await getPizzasData();
+
   return (
     <section id="menu" className="max-w-[1120px] mx-auto px-5 py-28 scroll-mt-16">
       <div className="flex flex-col items-center gap-24">
-        <motion.h2
-          initial={{ opacity: 0, y: 80 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4 }}
-          viewport={{
-            once: true,
-          }}
-          className="text-title text-[32px] font-extrabold leading-tight"
-        >
-          Cardápio
-        </motion.h2>
-
+        <h2 className="text-title text-[32px] font-extrabold leading-tight">Cardápio</h2>
         <div className="flex flex-wrap justify-center min-[1075px]:justify-between gap-x-12 gap-y-20">
-          {pizzas.map((pizza, i) => {
-            return (
-              <motion.div
-                key={pizza.slug}
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ duration: 0.4, delay: (i % 3) * 0.3, ease: 'easeInOut' }}
-                viewport={{
-                  once: true,
-                }}
-              >
-                <PizzaCard pizza={pizza} />
-              </motion.div>
-            );
+          {pizzas.map(pizza => {
+            return <PizzaCard key={pizza.slug} pizza={pizza} />;
           })}
         </div>
       </div>
